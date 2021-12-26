b57c693332337b475311f65f870b50a4fb407809
baileytowne <61771200+baileytowne@users.noreply.github.com>
2021-12-26 08:37:51 -1000

    my updated app

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    //ensure Amount is greater than 0
+    let eamount = document.getElementById("principal").value;
+    if (eamount <= 0){
+        alert("Amount must be greater than 0.");
+        return false;
+    }
+    //display principal
+    let eprincipal = document.getElementById("principal").value;
+    document.getElementById("rprincipal").innerHTML = "$" + eprincipal;
+    
+    //display interest rate
+    let erate = document.getElementById("rate").value;
+    document.getElementById("rrate").innerHTML = erate + "%";
+
+    //display final payoff year
+    let eyears = document.getElementById("years").value;
+    var todaysyear = new Date().getFullYear();
+    var fyear = parseInt(eyears) + parseInt(todaysyear);
+    document.getElementById('ryear').innerHTML = fyear;
+
+    //calclate interest
+    var einterest = "$" + ((parseInt(eprincipal) * (parseFloat(erate)/100) * parseInt(eyears))+parseInt(eprincipal));
+    document.getElementById("ramount").innerHTML = einterest;
+
+    //show results div
+    let element = document.getElementById("result");
+    element.removeAttribute("hidden");
+}
+
+function resetform()
+{
+    
+    //hide results div
+    let element = document.getElementById("result");
+    element.setAttribute("hidden", "hidden");
+    //clear results
+    document.getElementById("principal").value = "0";
+    document.getElementById("rate").value = "10.5";
+    document.getElementById('rateslider').innerHTML=rate.value;
+    document.getElementById("years").selectedIndex="0";
     
 }
-        
